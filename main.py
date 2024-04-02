baf58282e33ea4948903024c41f84daa09aa5654
Scriptyyy <scriptyyy@mail.ru>
2024-04-02 12:45:31 +0300

    что за зверь такой этот ваш пайгейм

--- main.py
+++ main.py
@@ -1,1 +1,30 @@
 import pygame
+import random
+
+WIDTH = 360  # ширина игрового окна
+HEIGHT = 480  # высота игрового окна
+FPS = 30  # частота кадров в секунду
+
+# создаем игру и окно
+pygame.init()
+pygame.mixer.init()  # для звука
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("My Game")
+clock = pygame.time.Clock()
+
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+
+running = True
+i = 1
+while running:
+    clock.tick(FPS)
+    i += 1
+    if i % 2 == 0:
+        screen.fill("BLACK")
+    else:
+        screen.fill("RED")
+    pygame.display.flip()
