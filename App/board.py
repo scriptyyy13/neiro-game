990a8e32c227f688c193fc3360828e25695031b3
Scriptyyy <scriptyyy@mail.ru>
2024-04-09 11:22:19 +0300

    added board

---
+++ App/board.py
@@ -0,0 +1,12 @@
+from typing import List
+
+import pygame
+import random
+
+
+class Board(object):
+    def __init__(self):
+        self.size = (520, 330) # размер по горизонтали и вертикали соответственно
+
+    def idk(self):
+        pass
