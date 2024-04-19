78629ce257d18aad29d0b51a95a4e43cef5549d5
Scriptyyy <scriptyyy@mail.ru>
2024-04-19 13:10:53 +0300

    добавлена сетка для змейки

--- App/snake.py
+++ App/snake.py
@@ -8,7 +8,8 @@ class Snake(object):
         self.length = SNAKE_START_LENGHT  # начальная длина
         self.vector = SNAKE_START_VECTOR  # начальный вектор движения
         self.speed = SNAKE_SPEED_MULTIPLIER  # увеличение скорости
-        self.coords = [[128, 300]]  # координаты головы
+        self.step = SNAKE_STEP
+        self.coords = [[5, 9]]  # координаты головы
 
     def set_vector(self):
         pass
@@ -19,5 +20,5 @@ class Snake(object):
 
     def draw_snake(self, coords, screen):
         for i in range(len(coords)):
-            pygame.draw.rect(screen, (0, 0, 0), pygame.Rect(coords[i][0], coords[i][1], 30, 30))
+            pygame.draw.rect(screen, (0, 0, 0), pygame.Rect(coords[i][0]*self.step, coords[i][1]*self.step, self.step, self.step))
 
