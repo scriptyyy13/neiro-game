435e0da7590640d2f8dbd642da64d50ccd76d40c
Scriptyyy <scriptyyy@mail.ru>
2024-05-07 11:19:40 +0300

    расписана функция ближайшего препятствия

--- App/api.py
+++ App/api.py
@@ -17,12 +17,22 @@ class Api(object):
 
     def is_near_obs(self, coords, walls):
         snake_head = coords[-1]
-        x_right = [snake_head[0] + 1, snake_head[1]] # тут везде используются массивы, так как массив из частей змейки состоит из массивов
-        x_left = [snake_head[0] - 1, snake_head[1]]
-        y_up = [snake_head[0], snake_head[1] - 1]
-        y_down = [snake_head[0], snake_head[1] + 1]
-        return [((x_right in coords) or x_right == [walls[0][1], snake_head[1]]), ((x_left in coords) or x_left == [0, snake_head[1]]),
-                ((y_up in coords) or y_up == [snake_head[0], 0]), ((y_down in coords)  or y_down == [snake_head[0], walls[1][1]])]
+        return [self.is_near(coords, walls, '+x'), self.is_near(coords, walls, '-x'), self.is_near(coords, walls, '+y'), self.is_near(coords, walls, '-y')]
+
+    def is_near (self, coords, walls, type):
+        snake_head = coords[-1]
+        if (type == '-x'):
+            next = [snake_head[0] - 1, snake_head[1]]
+            return ((next in coords) or next == [0, snake_head[1]])
+        elif (type == '+x'):
+            next = [snake_head[0] + 1, snake_head[1]]
+            return ((next in coords) or next == [walls[0][1], snake_head[1]])
+        elif (type == '+y'):
+            next = [snake_head[0], snake_head[1] - 1]
+            return ((next in coords) or next == [snake_head[0], 0])
+        elif (type == '-y'):
+            next = [snake_head[0], snake_head[1] + 1]
+            return ((next in coords) or next == [snake_head[0], walls[1][1]])
 
     def update(self, coords, food):
         pass
